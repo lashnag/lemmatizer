babdd613641e92fcacff4b52702e85c96a2ce1ec
a.lashnev <lexey2006.90@mail.ru>
2025-02-12 14:44:04 +0300

    Add logs to ELK

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import logging
 from fastapi.responses import JSONResponse
 from pymystem3 import Mystem
+from logger import init_logger
 
+init_logger()
 logging.getLogger().info("Lemmatizer run")
 server = FastAPI()
 mystem = Mystem()
@@ -13,5 +15,9 @@ class SentenceRequest(BaseModel):
 
 @server.post("/lemmatize")
 def lemmatize(request: SentenceRequest):
-    lemmas = mystem.lemmatize(request.word)
-    return JSONResponse(content={'lemmatized': lemmas[0]})
+    try:
+        lemmas = mystem.lemmatize(request.word)
+        return JSONResponse(content={'lemmatized': lemmas[0]})
+    except Exception as error:
+        logging.getLogger().error(f"Common error: {error}", exc_info=True)
+        raise HTTPException(status_code=500, detail="Произошла ошибка при лемматизации")
