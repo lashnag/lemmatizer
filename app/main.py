eda1dd5f7de860b76cfdfe4370fc1d347d6a3128
Alexey Lashnev <lashnag@gmail.com>
2025-03-30 13:05:17 +0300

    Use pymorphy2 instead pymystem3

--- app/main.py
+++ app/main.py
@@ -1,24 +1,28 @@
 import logging
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
-from pymystem3 import Mystem
 from logger import init_logger, request_headers
+from pymorphy2 import MorphAnalyzer
 
 init_logger()
 logging.getLogger().info("Lemmatizer run")
 server = FastAPI()
-mystem = Mystem()
+morph = MorphAnalyzer()
 
 @server.post("/lemmatize")
 async def lemmatize(request: Request):
     request_headers.set(dict(request.headers))
     try:
         body = await request.json()
-        lemmas = mystem.lemmatize(body.get("word"))
-        return JSONResponse(content={'lemmatized': lemmas[0]})
+        lemma = lemmatize_word(body.get("word"))
+        return JSONResponse(content={'lemmatized': lemma})
     except Exception as error:
         logging.getLogger().error(f"Common error: {error}", exc_info=True)
         raise HTTPException(status_code=500, detail="Произошла ошибка при лемматизации")
+
+def lemmatize_word(word: str):
+    parsed = morph.parse(word)[0]
+    return parsed.normal_form
 
 @server.get("/healthcheck")
 async def healthcheck():
